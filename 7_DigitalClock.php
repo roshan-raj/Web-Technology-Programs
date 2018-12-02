70600f80565890c7157d8fa07d979b3cae375e1c
Nikhil Amin <aminnikhil073@gmail.com>
2018-12-02 20:13:37 +0530

    Adding 7_DigitalClock.php

---
+++ 7_DigitalClock.php
@@ -0,0 +1,25 @@
+<!-- 
+	Write a PHP program to display a digital clock which displays
+	the current time of the server.
+-->
+<?php
+	function getTime() {
+		return date("g:i:s A", time());
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Digital Clock</title>
+	<meta http-equiv="Refresh" content="1">
+</head>
+<body style="background-color: #eeeeee;">
+	<div style="text-align: center; margin-top: 150px;">
+		<h2>Time:
+			<span style="color: red;">
+				<?php echo getTime() ?>
+			</span>
+		</h2>
+	</div>
+</body>
+</html>
